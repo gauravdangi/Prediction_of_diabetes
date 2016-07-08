fd5ba2a787de22ae7ed41f1c8c9c322288f57da7
Gaurav Dangi <gauravdangi96@gmail.com>
2016-07-08 13:26:43 +0530

    Update prediction of diabetes.R

--- prediction of diabetes.R
+++ prediction of diabetes.R
@@ -4,13 +4,18 @@ library(ggplot2)
 library(caret)
 library(data.table)
 library(Amelia)
+library(plotly)
 data("PimaIndiansDiabetes2")
 head(PimaIndiansDiabetes2)
 head(PimaIndiansDiabetes2)
 write.csv(PimaIndiansDiabetes2,"PimaIndiansDiabetes2.csv")
+data<-PimaIndiansDiabetes2
 
 #-------------Visualization data----------------------------
 library(ggplot2)
+attach(data)
+data$diabetes<-as.factor(data$diabetes)
+
 ggplot(data,aes(x=diabetes))+geom_bar(aes(fill=factor(diabetes)))
 
 cor(diabetes,glucose)   #it's 0.5
@@ -25,6 +30,11 @@ cor(diabetes,pedigree)  #it's 0.21
 ggplot(data,aes(x=pedigree,y=diabetes))+geom_point()+geom_smooth()
 
 ggplot(data=data,aes(x=age,y=glucose,col=diabetes))+geom_point()
+
+plot_ly(data=data,x=age,y=glucose,z=insulin,
+        color = diabetes,
+        type = "scatter3d")
+        
 diabetes<-as.numeric(diabetes)
 cor(mass,diabetes)
 
@@ -194,88 +204,3 @@ attach(data)
 data$diabetes=as.numeric(data$diabetes)-1
 head(data)
 cor(data)
-#--------------------------Exploring data-------------------------------------
-#Results are provided
-library(ggplot2)
-cor(diabetes,glucose)   #it's 0.5
-ggplot(data,aes(x=glucose,y=diabetes))+geom_point()+geom_smooth()
-cor(diabetes,pressure)  #it's 0.1926733
-ggplot(data,aes(x=pressure,y=diabetes))+geom_point()+geom_smooth()
-cor(diabetes,insulin)   #it's 0.3
-ggplot(data,aes(x=insulin,y=diabetes))+geom_point()+geom_smooth()
-cor(diabetes,age)       #it's 0.35
-ggplot(data,aes(x=age,y=diabetes))+geom_point()+geom_smooth()
-cor(diabetes,pedigree)  #it's 0.21
-ggplot(data,aes(x=pedigree,y=diabetes))+geom_point()+geom_smooth()
-
-ggplot(data=data,aes(x=age,y=glucose,col=diabetes))+geom_point()
-#Results are provided
-#-------------------------------------------------------
-#Logistic regression
-
-reg=glm(diabetes~.,data=data,family=binomial)
-summary(reg)
-head(reg$fitted.values)
-pred_diabetes=round(reg$fitted.values)
-comparison = ftable(pred_diabetes,data$diabetes)
-comparison
-accuracy = sum(diag(comparison))/sum(comparison)*100
-accuracy  #78.31633% accuracy
-#--------------------------------------------------------
-#Using Support vector machine algorithm(SVM)
-
-library(e1071)
-reg2=svm(diabetes~.,data=data,cost = 100,gamma = 1)
-reg2
-pred_v2=round(fitted(reg2))
-pred_v2  
-comparision2=ftable(pred_v2,data$diabetes) 
-comparision2
-accuracy2=sum(diag(comparision2))/sum(comparision2)*100
-accuracy2   #100% accuracy
-accu=(1-mean(data$diabetes!=pred_v2))*100  #we may also check accuracy by this way
-accu
-#--------------------------------------------------------
-#using neural networking
-#hidden layer=1
-library(neuralnet)
-nn=neuralnet(diabetes~pregnant+glucose+triceps+insulin+mass+pedigree+age,data=data,hidden=1,linear.output = FALSE,err.fct="ce")
-nn
-plot(nn)
-
-nn$weights
-nn$net.result
-pred_v3=ifelse(nn$net.result[[1]]>0.5,1,0)
-comparison3=table(pred_v3,data$diabetes)
-comparison3
-accuracy3=sum(diag(comparison3))/sum(comparison3)*100
-accuracy3
-#hidden layer=2
-nn2=neuralnet(diabetes~pregnant+glucose+triceps+insulin+mass+pedigree+age,data=data,hidden=2,linear.output = FALSE,err.fct="ce")
-nn2
-plot(nn2)
-nn2$weights
-nn2$net.result
-pred_v3=ifelse(nn2$net.result[[1]]>0.5,1,0)
-comparison4=table(pred_v3,data$diabetes)
-comparison4
-accuracy4=sum(diag(comparison3))/sum(comparison3)*100
-accuracy4
-#-----------------------------------------------------------
-#Desision tree
-# Classification Tree with rpart
-library(rpart)
-# grow tree 
-fit=rpart(diabetes~.,data=data,method = "class")
-fit
-printcp(fit) # display the results 
-plotcp(fit) # visualize cross-validation results 
-summary(fit) # detailed summary of splits
-
-# plot tree 
-plot(fit, uniform=TRUE, main="Classification Tree for Diabetes")
-text(fit, use.n=TRUE, all=TRUE, cex=.8)
-
-# create attractive postscript plot of tree 
-post(fit, file = "c:/tree.ps", title = "Classification Tree for diabetes")
->>>>>>> origin/master
